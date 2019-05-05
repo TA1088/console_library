16d18b00f3ff0c634e3221cc7788293226e74282
TA1088 <zolsen8806@gmail.com>
2019-05-05 00:42:25 -0600

    Initialized with Library and Book

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using console_library.Models;
+
+namespace console_library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    Book myBook = new Book();
+        //    myBook.Author = "Shel Silverstein";
+        //    myBook.Title = "Where the Sidewalk Ends";
+        Book WhereTheSidewalkEnds = new Book("Where the Sidewalk Ends", "Shel Silverstein");
+        
+
+        }
+    }
+}
